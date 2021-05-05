92179c6ba4fa9a15bd90c27ae4e916a32b2ccb4a
Casey Burleyson <casey.burleyson@pnnl.gov>
2021-05-05 16:06:37 -0700

    Added BA processing script and updated readme

--- Readme.md
+++ Readme.md
@@ -30,6 +30,7 @@ Burleyson, C.D., A. Rahman, J.S. Rice, A.D. Smith, and N. Voisin (2021). Support
 | 2 | `Process_ComEd_Monthly_Load_Profiles.m` | Process monthly average weekday and weekend ComEd load profiles |
 | 3 | `Process_Raw_EIA_Regional_Hourly_Load_Data.m` | Process the raw EIA regional hourly load data into Matlab files |
 | 4 | `Process_HIFLD_Control_Area_Shapefiles.m` | Process the shapefiles for HIFLD control areas into Matlab files |
+| 5 | `Process_Raw_EIA_Balancing_Authority_Hourly_Load_Data` | Process the raw EIA balancing authority hourly load data into Matlab files |
 
 3. Run the following scripts in the `figures` directory to reproduce our figures and compare your outputs to those from the publication.
 
--- workflow/Process_Raw_EIA_Balancing_Authority_Hourly_Load_Data.m
+++ workflow/Process_Raw_EIA_Balancing_Authority_Hourly_Load_Data.m
@@ -1,11 +1,12 @@
-% Process_Raw_EIA_Regional_Hourly_Load_Data.m
+% Process_Raw_EIA_Balancing_Authority_Hourly_Load_Data.m
 % 20210505
 % Casey D. Burleyson
 % Pacific Northwest National Laboratory
 
-% Take the raw EIA hourly load data by region and convert it from Excel files into .mat files. 
+% Take the raw EIA hourly load data by balancing authority and convert it from Excel files into .mat files. 
 % The "data_directory" variable should  be set to the path of the input data that you downloaded 
-% in Step 1 of the workflow.
+% in Step 1 of the workflow. The output file format is given below. All times are in UTC. 
+% Missing values are reported as NaN in the .mat output files. 
 
 warning off all; clear all; close all; 
 
@@ -24,38 +25,37 @@ data_directory = '/Users/burl878/OneDrive - PNNL/Documents/Papers/2021_Burleyson
 %              BEGIN PROCESSING SECTION               %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Make a list of all of the regional files in the input directory:
-input_files = dir([data_directory,'input_data/EIA_Regional_Hourly_Load/Raw/Region_*.xlsx']);
+input_files = dir([data_directory,'input_data/EIA_Balancing_Authority_Hourly_Load/Raw/*.xlsx']);
 
-% Loop over each of the files in the filelist:
+% Loop over each of the files and extract the variables of interest:
 for file = 1:size(input_files,1)
-    
-    % Extract the filename and region code:
+    % Read in the raw .xlsx file:
     filename = input_files(file,1).name;
-    Region = filename(1,[8:(size(filename,2)-5)]);
-
-    % Read in the data from the Excel file:
-    [~,~,Raw_Data] = xlsread([data_directory,'input_data/EIA_REgional_Hourly_Load/Raw/',filename],'Published Hourly Data');
+    [~,~,Raw_Data] = xlsread([data_directory,'input_data/EIA_Balancing_Authority_Hourly_Load/Raw/',filename],'Published Hourly Data');
     Raw_Data(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),Raw_Data)) = {''};
     
-    % Loop over all the rows (hours) in the data and extract the actual and forecast demand:
+    % Extract the balancing authority code:
+    BA_Code = char(Raw_Data{2,1});
+    
+    % Loop over the rows and extract the variables of interest:
     counter = 0;
     for row = 2:size(Raw_Data,1)
         counter = counter + 1;
         
-        % Convert the date from Excel time into Matlab's datenumber format:
-        Data(counter,1) = datenum(Raw_Data{row,5} + 693960);
-        Data(counter,2:7) = datevec(Raw_Data{row,5} + 693960);
-                
-        % If there is a valid demand value then extract it:
-        if isempty(Raw_Data{row,8}) == 0
-           Data(counter,6) = Raw_Data{row,8};
+        % Convert the UTC date from the Excel time format to the Matlab date number format:
+        Data(counter,1) = datenum(Raw_Data{row,2} + 693960);
+        Data(counter,2:7) = datevec(Raw_Data{row,2} + 693960);
+        
+        % If there is a valid adjusted demand value then extract it:
+        if isempty(Raw_Data{row,15}) == 0
+           Data(counter,6) = Raw_Data{row,15};
         else
            Data(counter,6) = NaN.*0;
         end
         
         % If there is a valid forecast demand value then extract it:
-        if isempty(Raw_Data{row,7}) == 0
-           Data(counter,7) = Raw_Data{row,7};
+        if isempty(Raw_Data{row,8}) == 0
+           Data(counter,7) = Raw_Data{row,8};
         else
            Data(counter,7) = NaN.*0;
         end
@@ -63,7 +63,7 @@ for file = 1:size(input_files,1)
         % Compute the forecast error (Forecast - Observed):
         Data(counter,8) = Data(counter,7) - Data(counter,6);
     end
-    
+        
     % Save the ouput as a .mat file with the following file format:
     % C1 = Matlab datenumber
     % C2 = Year
@@ -73,9 +73,11 @@ for file = 1:size(input_files,1)
     % C6 = Actual demand in MWh
     % C7 = Forecast demand in MWh
     % C8 = Forecast error in MWh 
-    save([data_directory,'input_data/EIA_REgional_Hourly_Load/Processed/',Region,'_Hourly_Load_Data.mat'],'Data','Region');
-    clear filename Region Raw_Data counter row Data
+    save([data_directory,'input_data/EIA_Balancing_Authority_Hourly_Load/Processed/',BA_Code,'_Hourly_Load_Data.mat'],'Data');
+       
+    clear Data filename Raw_Data counter row BA_Code
 end
+clear input_files file
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %               END PROCESSING SECTION                %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -85,7 +87,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                BEGIN CLEANUP SECTION                %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clear data_directory input_files file
+clear data_directory
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                 END CLEANUP SECTION                 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
